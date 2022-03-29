54cb00fedcdbe8374b0e4b5ea06698d4ba77dcb8
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2022-03-29 08:31:17 +0300

    feat: Add function right_rotate32 to perform bitwise right rotation

--- sha256.py
+++ sha256.py
@@ -3,3 +3,13 @@ def add32(*args):
     Adds the numbers in the list of args and returns (sum % s ^ 32)
     """
     return sum(args) % (2**32)
+
+
+def right_rotate32(value, count):
+    """
+    Bitwise right rotation of value by {count} places
+    """
+    assert value < 2 ** 32, "x is too large. Did you use + instead of add32 somewhere?"
+    right_part = value >> count
+    left_part = value << (32 - count)
+    return add32(left_part, right_part)
--- test_sha256.py
+++ test_sha256.py
@@ -1,4 +1,4 @@
-from .sha256 import add32
+from .sha256 import add32, right_rotate32
 
 
 def test_add32_one():
@@ -9,3 +9,12 @@ def test_add32_two():
 
 def test_add32_three():
     assert add32(3050487260, 3710144918) == 2465664882
+
+def test_right_rotate32_one():
+    assert right_rotate32(2, 1) == 1
+
+def test_right_rotate32_two():
+    assert right_rotate32(1, 1) == 2147483648
+
+def test_right_rotate32_three():
+    assert right_rotate32(2919882184, 31) == 1544797073
